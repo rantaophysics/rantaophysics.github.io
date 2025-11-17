7f459d498a8257e45564f01c464e058d4b055444
rantaophysics <105693440+rantaophysics@users.noreply.github.com>
2025-11-17 03:52:53 +0000

    Deploying to gh-pages from @ rantaophysics/rantaophysics.github.io@f31a53f0b785e4cf67e4d202938c6b1c2c325b2e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/about/";
+            window.location.href = "/";
           },
         },{id: "nav-publications",
           title: "publications",
